5db2efacb1b101071077ef20d993fa8a42a64a00
ymwang <maxim19940116@126.com>
2019-08-19 00:00:18 +0800

    1

--- src/views/main/components/tours.vue
+++ src/views/main/components/tours.vue
@@ -1,15 +1,20 @@
 <template>
-  <el-row>
-    <el-col :md="8" :sm="12" :xs="24">
-      <spot-card></spot-card>
-    </el-col>
-    <el-col :md="8" :sm="12" :xs="24">
-      <spot-card></spot-card>
-    </el-col>
-    <el-col :md="8" :sm="12" :xs="24">
-      <spot-card></spot-card>
-    </el-col>
-  </el-row>
+  <div>
+    <div class="tour-title">
+      <h3>当季热门度假</h3>
+    </div>
+    <el-row>
+      <el-col :md="8" :sm="12" :xs="24">
+        <spot-card></spot-card>
+      </el-col>
+      <el-col :md="8" :sm="12" :xs="24">
+        <spot-card></spot-card>
+      </el-col>
+      <el-col :md="8" :sm="12" :xs="24">
+        <spot-card></spot-card>
+      </el-col>
+    </el-row>
+  </div>
 </template>
 
 <script lang="ts">
@@ -30,5 +35,17 @@ export default class Tours extends Vue {}
   padding-left: 10px;
   padding-right: 10px;
   padding-bottom: 10px;
+}
+.tour-title {
+  text-align: left;
+  border-bottom: 2px solid #00afc7;
+  height: 45px;
+  margin-bottom: 15px;
+}
+.tour-title h3 {
+  line-height: 45px;
+  font: 23px/45px "\5FAE\8F6F\96C5\9ED1";
+  font-family: "Hiragino Sans GB", "\5FAE\8F6F\96C5\9ED1";
+  color: #00afc7;
 }
 </style>
